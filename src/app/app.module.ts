7400899cd579b37e0c682f2be9ed9f87e374b283
Solomoni <soni@byteworks.com.ng>
2022-12-19 18:10:14 +0100

    form input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { JobListingsComponent } from './job-listings/job-listings.component';
 import { AppliedJobsComponent } from './applied-jobs/applied-jobs.component';
+import { FormSearchComponent } from './form-search/form-search.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { AppliedJobsComponent } from './applied-jobs/applied-jobs.component';
     HeaderComponent,
     FooterComponent,
     JobListingsComponent,
-    AppliedJobsComponent
+    AppliedJobsComponent,
+    FormSearchComponent
   ],
   imports: [
     BrowserModule,
