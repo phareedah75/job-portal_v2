be110d0e621f0641fd748f95b447db37f1d7cfd7
Solomoni <soni@byteworks.com.ng>
2023-01-12 10:42:39 +0100

    company information

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppliedJobsComponent } from './applied-jobs/applied-jobs.component';
 import { CompanyInfoComponent } from './company-info/company-info.component';
+import { JobDescriptionComponent } from './job-description/job-description.component';
 import { JobListingsComponent } from './job-listings/job-listings.component';
 import { JobSelectComponent } from './job-select/job-select.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PostJobComponent } from './post-job/post-job.component';
 
 const routes: Routes = [
   {
@@ -30,6 +32,16 @@ const routes: Routes = [
   {
     path: "company-info",
     component: CompanyInfoComponent
+  },
+
+  {
+    path: "post-job",
+    component: PostJobComponent
+  },
+
+  {
+    path: "job-description",
+    component: JobDescriptionComponent
   },
 ];
 
